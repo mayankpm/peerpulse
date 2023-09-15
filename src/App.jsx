5d66de43f60d95c5c198f27a4749a50a0eebc156
mayankpm <92316639+mayankpm@users.noreply.github.com>
2023-09-15 19:10:35 +0530

    removed unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,8 @@
 function App() {
 
   return (
-    <div className="w-screen h-screen bg-red-300 underline">
-      Hello World
-      <label className="swap swap-flip text-9xl">
-  
-  {/* this hidden checkbox controls the state */}
-  <input type="checkbox" />
-  
-  <div className="swap-on">😈</div>
-  <div className="swap-off">😇</div>
-</label>
-    </div>
+<>
+</>
     
   )
 }
