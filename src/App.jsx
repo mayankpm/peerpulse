27c083534ea884429456223c5995baa6e682b77d
Pavvan Pradeep <pavvanpradeep@gmail.com>
2023-09-16 00:33:16 +0530

    Added Profile Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from './Components/Login/Loginpage'
 import FLdata from "./Components/FLdata/FLdata"
 import ED from "./Components/EmployerData/ED"
 import Card from "./Components/Homepage/Card"
+import Profile from "./Components/Profile/Profile"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Login/>
       <FLdata/>
       <Card name="Mayank P M" title="AI/ML Expert"/>
+      <Profile/>
     </>
     
   )
