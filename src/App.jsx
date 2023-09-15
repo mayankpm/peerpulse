2b3cbfc431e56b3873ead7e9164d3594d68f3555
Pavvan Pradeep <pavvanpradeep@gmail.com>
2023-09-15 23:02:41 +0530

    Added props to cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Login from './Components/Login/Loginpage'
 import FLdata from "./Components/FLdata/FLdata"
 import ED from "./Components/EmployerData/ED"
 import Card from "./Components/Homepage/Card"
-import img from "./Assets/optimus.jpg"
 
 function App() {
 
@@ -11,7 +10,7 @@ function App() {
     <>
       <Login/>
       <FLdata/>
-      <Card name="Mayank P M" title="AI/ML Expert" dp={img}/>
+      <Card name="Mayank P M" title="AI/ML Expert"/>
     </>
     
   )
