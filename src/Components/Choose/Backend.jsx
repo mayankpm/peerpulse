993f6b013a03e9c7657db938c9fb9f42ac77b32a
mayankpm <92316639+mayankpm@users.noreply.github.com>
2023-09-16 06:45:09 +0530

    final

---
+++ src/Components/Choose/Backend.jsx
@@ -0,0 +1,20 @@
+const backendData = [
+    {
+      name: 'John Doe',
+      title: 'Software Engineer',
+      dp: 'andrew-neel.jpg',
+    },
+    {
+      name: 'Jane Smith',
+      title: 'Web Developer',
+      dp: 'faizur-rehman.jpg',
+    },
+    {
+      name: 'Samuel Johnson',
+      title: 'Data Scientist',
+      dp: 'andrew-neel.jpg',
+    },
+  ];
+  
+  export default backendData;
+  
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -4,14 +4,18 @@ import Desc from './Desc'
 import img from "../../assets/faizur-rehman.jpg"
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
-export default function Profile() {
+import Price from './Price'
+
+export default function Profile(props) {
+  const { name,img } = props;
   return (
     <>
       <Navbar/>
         <div className='flex justify-center align-center'>
-            <Desc name={"Nikhil John"} desc={"I have special experience in Graph Theory, can help you understand any algorithm and ensure your success."}/>
+            <Desc name={name} desc={"I have special experience in Graph Theory, can help you understand any algorithm and ensure your success."}/>
             <Image dp={img}/>
         </div>
+      <Price/>
         <Footer/>
     </>
   )
